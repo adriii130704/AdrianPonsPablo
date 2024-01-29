c9597bc4374b1d50a497d6da5a5e4e6fe5388d15
adriii130704 <157619108+adriii130704@users.noreply.github.com>
2024-01-29 16:33:51 +0100

    Update Calculadora.java

--- GitHub/Calculadora.java
+++ GitHub/Calculadora.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public class Calculadora {
 
-    public static void sumayResta(int num1,/*dato añadido por mi */String simbolo, int num2) {
-        //ehh cambiado la suma por un if para elegir entre suma y resta
-        if(simbolo=="+"){
-        int suma = num1 + num2;
-        System.out.println("El total de la suma entre " + num1 + " y " + num2 + " es: " + suma);
-        }else if(simbolo=="-"){
-        int resta = num1 - num2;
-        System.out.println("El total de la resta entre " + num1 + " y " + num2 + " es: " + resta);
+    public static void sumayResta(int num1, String simbolo, int num2) {
+        // ehh cambiado la suma por un if para elegir entre suma y resta
+        switch (simbolo) {
+            case "+":
+                System.out.println("la suma entre " + num1 + " y " + num2 + " es: " + (num1 + num2));
+            case "-":
+                System.out.println("la resta entre " + num1 + " y " + num2 + " es: " + (num1 - num2));
         }
     }
 
@@ -17,11 +16,12 @@ public class Calculadora {
         Scanner sc = new Scanner(System.in);
         System.out.print("introducel el primer numero: ");
         int num1 = sc.nextInt();
-        System.out.print("Pon simbolo suma(+) o resta(-)");
-        String simbolo = sc.nextLIne();
+        System.out.print("Pon simbolo suma(+) o resta(-): ");
+        String simbolo = sc.nextLine();
+        sc.nextLine();
         System.out.print("introducel el segundo numero: ");
         int num2 = sc.nextInt();
-
-        sumayResta(num1,simbolo, num2);
+        sumayResta(num1, simbolo, num2);
+        sc.close();
     }
 }
